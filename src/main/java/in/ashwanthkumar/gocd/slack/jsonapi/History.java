be9d961f242ad60c9fdbc54abb5f061c78ab4eda
Eric Kidd <git@randomhacks.net>
2015-07-09 00:34:34 -0400

    Log from History at debug priority
    We don't need anything more detailed than this.

--- src/main/java/in/ashwanthkumar/gocd/slack/jsonapi/History.java
+++ src/main/java/in/ashwanthkumar/gocd/slack/jsonapi/History.java
@@ -58,14 +58,17 @@ public class History {
      * Find the most recent run of the specified stage _before_ this one.
      */
     public Stage previousRun(int pipelineCounter, String stageName, int stageCounter) {
-        LOG.info(String.format("Looking for stage before %d/%s/%d",
-                               pipelineCounter, stageName, stageCounter));
+        LOG.debug(String.format("Looking for stage before %d/%s/%d",
+                                pipelineCounter, stageName, stageCounter));
+
+        // Note that pipelines and stages are stored in reverse
+        // chronological order.
         for (int i = 0; i < pipelines.length; i++) {
             Pipeline pipeline = pipelines[i];
             for (int j = 0; j < pipeline.stages.length; j++) {
                 Stage stage = pipeline.stages[j];
-                LOG.info(String.format("Checking %d/%s/%d",
-                                       pipeline.counter, stage.name, stage.counter));
+                LOG.debug(String.format("Checking %d/%s/%d",
+                                        pipeline.counter, stage.name, stage.counter));
 
                 if (stage.name.equals(stageName)) {
 
